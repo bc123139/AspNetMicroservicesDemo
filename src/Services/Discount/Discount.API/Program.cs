7a1455904f1f45d74f6abf90d4d3b6571449be9a
Usman Tahir <Usman.tahir@synergy-it.com>
2022-08-07 12:52:32 +0500

    Create Coupon Table in the DiscountDb of PostgreSQL Database with pgAdmin Manage

--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -21,3 +21,15 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
+//CREATE TABLE Coupon(
+//		ID SERIAL PRIMARY KEY         NOT NULL,
+//		ProductName     VARCHAR(24) NOT NULL,
+//		Description     TEXT,
+//		Amount          INT
+//	);
+
+//INSERT INTO Coupon (ProductName, Description, Amount) VALUES('IPhone X', 'IPhone Discount', 150);
+
+//INSERT INTO Coupon (ProductName, Description, Amount) VALUES('Samsung 10', 'Samsung Discount', 100);

691fe4b3d8657b6278b4b655eb6637d930b52976
Usman Tahir <Usman.tahir@synergy-it.com>
2022-08-27 16:19:33 +0500

    Developing Ordering.Application Layer - Application Command Features - Checkout

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderCommandHandler.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Contracts.Infrastructure;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Application.Models;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Application.Features.Orders.Commands.CheckoutOrder
+{
+    public class CheckoutOrderCommandHandler : IRequestHandler<CheckoutOrderCommand, int>
+    {
+        private readonly IMapper _mapper;
+        private readonly IOrderRepository _orderRepository;
+        private readonly IEmailService _emailService;
+        private readonly ILogger<CheckoutOrderCommandHandler> _logger;
+
+        public CheckoutOrderCommandHandler(IMapper mapper, IOrderRepository orderRepository, IEmailService emailService, ILogger<CheckoutOrderCommandHandler> logger)
+        {
+            _mapper = mapper;
+            _orderRepository = orderRepository;
+            _emailService = emailService;
+            _logger = logger;
+        }
+        public async Task<int> Handle(CheckoutOrderCommand request, CancellationToken cancellationToken)
+        {
+            var orderEntity = _mapper.Map<Order>(request);
+            var newOrder = await _orderRepository.AddAsync(orderEntity);
+            _logger.LogInformation($"Order {newOrder.Id} is successfully created.");
+            await SendMail(newOrder);
+            return newOrder.Id;
+        }
+
+        private async Task SendMail(Order order)
+        {
+            var email = new Email() { To = "utahir604@gmail.com", Body = $"Order was created.", Subject = "Order was created" };
+
+            try
+            {
+                await _emailService.SendEmail(email);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Order {order.Id} failed due to an error with the mail service: {ex.Message}");
+            }
+        }
+    }
+}

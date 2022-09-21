0d04b022f9458b4e40f24218b19b274cff237d66
Usman Tahir <Usman.tahir@synergy-it.com>
2022-09-22 00:15:53 +0500

    Refactoring of AspnetBasics Shopping Web Application Microservices

---
+++ src/WebApps/AspnetRunBasics/Models/BasketModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace AspnetRunBasics.Models
+{
+    public class BasketModel
+    {
+        public string UserName { get; set; } = null!;
+        public List<BasketItemExtendedModel> Items { get; set; } = new List<BasketItemExtendedModel>();
+        public decimal TotalPrice { get; set; }
+    }
+}
---
+++ src/WebApps/AspnetRunBasics/Models/CatalogModel.cs
@@ -0,0 +1,13 @@
+﻿namespace AspnetRunBasics.Models
+{
+    public class CatalogModel
+    {
+        public string Id { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public string Category { get; set; } = null!;
+        public string Summary { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string ImageFile { get; set; } = null!;
+        public decimal Price { get; set; }
+    }
+}

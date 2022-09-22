4aea9af6c27288b0875ccaee47ebbe7affadb3b5
Usman Tahir <Usman.tahir@synergy-it.com>
2022-09-22 23:29:04 +0500

    Register Http Client Factory for Consuming Api Gateway in AspnetBasics Shopping

--- src/ApiGateways/Shopping.Aggregator/Extensions/HttpClientExtensions.cs
+++ src/ApiGateways/Shopping.Aggregator/Extensions/HttpClientExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using System.Net.Http.Headers;
+using System.Text.Json;
 
 namespace Shopping.Aggregator.Extensions
 {
@@ -12,6 +13,24 @@ namespace Shopping.Aggregator.Extensions
             var dataAsString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
 
             return JsonSerializer.Deserialize<T>(dataAsString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true })!;
+        }
+
+        public static Task<HttpResponseMessage> PostAsJson<T>(this HttpClient httpClient, string url, T data)
+        {
+            var dataAsString = JsonSerializer.Serialize(data);
+            var content = new StringContent(dataAsString);
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            return httpClient.PostAsync(url, content);
+        }
+
+        public static Task<HttpResponseMessage> PutAsJson<T>(this HttpClient httpClient, string url, T data)
+        {
+            var dataAsString = JsonSerializer.Serialize(data);
+            var content = new StringContent(dataAsString);
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            return httpClient.PutAsync(url, content);
         }
     }
 }
